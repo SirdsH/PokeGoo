2fdd76ddaf2b503cb1963deb415e5e703f0e9ec9
SirdsH <svatopluk.houserek@educanet.cz>
2022-02-01 12:44:02 +0100

    working search

--- index/js.js
+++ index/js.js
@@ -1,18 +1,42 @@
+let arr = [];
+
 window.onload = () => {
+    let input = document.getElementById('search')
+    const ctr = document.getElementById("ctr")
+    input.addEventListener('keyup', e => {
+        if(e.key === "Enter") {
+            f(arr, ctr, input.value)
+        }
+    })
     let writeOut = document.getElementById('writeOut');
     fetch('https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/pokedex.json')
         .then((response) => response.json())
         .then((data) => {
-            for (let i = 0; i < data.slice(0,7).length; i++) {
-                let card = document.createElement('div');
-                card.classList.add('card');
-                card.innerHTML = ` 
-   <img src="${data[i].hires}" class="card-img-top w-25%">
+            arr = data
+            f(data, ctr)
+
+        });
+
+}
+
+function f(data, ctr, filter = "") {
+    let count = 0
+    ctr.innerHTML = ""
+    for (let i = 0; i < data.length - 1; i++) {
+        if (data[i].name.english.toLowerCase().startsWith(filter)) {
+            count++
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.classList.add('small-card');
+            card.innerHTML = ` 
+   <img src="${data[i].hires}" class="card-img-top">
      <div class="card-body">
           <h5 class="card-title">${data[i].name.english}</h5>
          <p class="card-text">${data[i].description}</p>
      </div>`
-                document.body.appendChild(card);
-            }
-        });
+            arr.push(data);
+            ctr.appendChild(card);
+            if(count === 8) break
+        }
+    }
 }
